b9e5f237b235d253912837088ad47a58fb50b53c
ayles1 <ayles1113@gmail.com>
2022-10-19 22:49:35 +0300

    19.10.22 progress

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
-const menuItemAboutMe = document.querySelectorAll(".page-one__text");
+const menuItemAboutMe = document.querySelectorAll(".menu__item")[0];
+const scrollTargetAboutMe = document.querySelector(".page-two");
 
 menuItemAboutMe.onclick = function () {
-  console.log("Hello world");
+  scrollTargetAboutMe.scrollIntoView({ block: "start", behavior: "smooth" });
+};
+
+const menuItemSkills = document.querySelectorAll(".menu__item")[1];
+const scrollTargetSkills = document.querySelector(".page-three");
+
+menuItemSkills.onclick = function () {
+  scrollTargetSkills.scrollIntoView({ block: "start", behavior: "smooth" });
+};
+
+const menuItemPortfolio = document.querySelectorAll(".menu__item")[2];
+
+menuItemPortfolio.onclick = function () {
+  alert("Hello world");
 };
