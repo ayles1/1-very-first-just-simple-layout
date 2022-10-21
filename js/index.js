6037898b626b7ced237e49cae75096d5fbe9909b
ayles1 <ayles1113@gmail.com>
2022-10-21 20:07:29 +0300

    80% done

--- js/index.js
+++ js/index.js
@@ -13,7 +13,53 @@ menuItemSkills.onclick = function () {
 };
 
 const menuItemPortfolio = document.querySelectorAll(".menu__item")[2];
+const scrollTargetPortfolio = document.querySelector(".page-four");
 
 menuItemPortfolio.onclick = function () {
-  alert("Hello world");
+  scrollTargetPortfolio.scrollIntoView({ block: "start", behavior: "smooth" });
 };
+
+//Burger script---------------------------------------------------
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+const hambContainer = document.querySelector(".hamb__container");
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  hambContainer.classList.toggle("opened");
+
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+//----------------------------------------------------------------
